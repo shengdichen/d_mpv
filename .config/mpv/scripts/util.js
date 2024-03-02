7f5ed3b5e53018fa6e29c7fab2bf06d844fbeeb2
shc <sh@c>
2024-03-02 18:08:41 +0100

    feat(osc): 1st impl'n toggle-osc

--- .config/mpv/scripts/util.js
+++ .config/mpv/scripts/util.js
@@ -5,4 +5,20 @@ function print_osd (text, duration) {
   mp.osd_message(text, duration)
 }
 
+function _is_visible_by_default_osc () {
+  var opt = { visibility: 'never' }
+  mp.options.read_options(opt, 'osc')
+  return opt.visibility != 'never'
+}
+var is_visible_osc = _is_visible_by_default_osc()
+function toggle_osc () {
+  if (is_visible_osc) {
+    mp.command('no-osd script-message osc-visibility never')
+  } else {
+    mp.command('no-osd script-message osc-visibility always')
+  }
+  is_visible_osc = !is_visible_osc
+}
+
 module.exports.print_osd = print_osd
+module.exports.toggle_osc = toggle_osc
