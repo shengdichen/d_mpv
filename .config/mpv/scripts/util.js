8f8f9dc6f47eda33661d693a13dec7212044a6dc
shc <sh@c>
2024-03-02 23:14:14 +0100

    refactor(osc): rewrite in object-style

--- .config/mpv/scripts/util.js
+++ .config/mpv/scripts/util.js
@@ -9,23 +9,35 @@ function mp_run_command (fragments) {
   mp.commandv.apply(null, fragments)
 }
 
-function _is_visible_by_default_osc () {
-  var opt = { visibility: 'never' }
-  mp.options.read_options(opt, 'osc')
-  return opt.visibility != 'never'
-}
-var is_visible_osc = _is_visible_by_default_osc()
-function toggle_osc () {
-  var frags_base = ['no-osd', 'script-message', 'osc-visibility']
-  var frags_disable = frags_base.concat('never')
-  var frags_enable = frags_base.concat('always')
-  if (is_visible_osc) {
-    mp_run_command(frags_disable)
-  } else {
-    mp_run_command(frags_enable)
+function Osc () {
+  function _is_visible_by_default () {
+    var opt = { visibility: 'never' }
+    mp.options.read_options(opt, 'osc')
+    return opt.visibility != 'never'
   }
-  is_visible_osc = !is_visible_osc
+  this._is_visible = _is_visible_by_default()
+
+  var frags_base = ['no-osd', 'script-message', 'osc-visibility']
+  this._frags_disable = frags_base.concat('never')
+  this._frags_enable = frags_base.concat('always')
+
+  this.disable = function () {
+    mp_run_command(this._frags_disable)
+  }
+
+  this.enable = function () {
+    mp_run_command(this._frags_enable)
+  }
+
+  this.toggle = function () {
+    if (this._is_visible) {
+      this.disable()
+    } else {
+      this.enable()
+    }
+    this._is_visible = !this._is_visible
+  }
 }
 
 module.exports.print_osd = print_osd
-module.exports.toggle_osc = toggle_osc
+module.exports.osc = new Osc()
