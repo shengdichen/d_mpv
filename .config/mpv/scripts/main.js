18120bcaa88067281759d114ea8ad47a55da0588
shc <sh@c>
2024-03-03 02:08:44 +0100

    feat(bind-to-js): first batch audio-video

--- .config/mpv/scripts/main.js
+++ .config/mpv/scripts/main.js
@@ -3,6 +3,9 @@ var util = require('./util')
 function Main () {
   this.rebind = function () {
     mp.add_forced_key_binding('o', function () { util.osc.toggle() })
+
+    util.video.bind()
+    util.audio.bind()
   }
 }
 var main = new Main()
--- .config/mpv/scripts/util.js
+++ .config/mpv/scripts/util.js
@@ -1,3 +1,11 @@
+function MiscUtil () {
+  this.format_integer = function (num) {
+    return (num < 0 ? '' : '+') + num
+  }
+}
+var misc_util = new MiscUtil()
+module.exports.misc_util = misc_util
+
 function MpvUtil () {
   this.print_osd = function (text, duration) {
     if (!duration) {
@@ -8,6 +16,34 @@ function MpvUtil () {
 
   this.run = function (fragments) {
     mp.commandv.apply(null, fragments)
+  }
+
+  this.get_prop = function (prop, type, def) {
+    if (type === 'bool') {
+      return mp.get_property_bool(prop, def)
+    }
+    if (type === 'num') {
+      return mp.get_property_number(prop, def)
+    }
+    return mp.get_property(prop)
+  }
+
+  this.set_prop = function (prop, val, type) {
+    if (type === 'bool') {
+      return mp.set_property_bool(prop, val)
+    }
+    if (type === 'num') {
+      return mp.set_property_number(prop, val)
+    }
+    return mp.set_property(prop)
+  }
+
+  this.cycle = function (item, values) {
+    if (!values) {
+      this.run(['cycle', item])
+    } else {
+      this.run(['cycle-values', item].concat(values))
+    }
   }
 }
 var mpv_util = new MpvUtil()
@@ -43,3 +79,43 @@ function Osc () {
   }
 }
 module.exports.osc = new Osc()
+
+function Audio () {
+  this.volume = function (incr) {
+    return function () {
+      var vol_prev = mpv_util.get_prop('volume', type = 'num')
+      var vol_next = vol_prev + incr
+      mpv_util.set_prop('volume', vol_next, type = 'num')
+      mpv_util.print_osd(
+        'volume> ' + vol_next + ' [' + vol_prev + misc_util.format_integer(incr) + ']'
+      )
+    }
+  }
+
+  this.mute = function () {
+    mpv_util.cycle('mute')
+    mpv_util.print_osd('mute> ' + (mpv_util.get_prop('mute', type = 'bool') ? 'T' : 'F'))
+  }
+
+  this.bind = function () {
+    mp.add_key_binding('9', this.volume(-1))
+    mp.add_key_binding('(', this.volume(-7))
+    mp.add_key_binding('0', this.volume(+1))
+    mp.add_key_binding(')', this.volume(+7))
+
+    mp.add_key_binding('m', this.mute)
+  }
+}
+module.exports.audio = new Audio()
+
+function Video () {
+  this.bind = function () {
+    mp.add_key_binding('SPACE', function () { mpv_util.cycle('pause') })
+    mp.add_key_binding('f', function () { mpv_util.cycle('fullscreen') })
+    mp.add_key_binding('_', function () { mpv_util.cycle('video') })
+    mp.add_key_binding('Ctrl+r', function () {
+      mpv_util.cycle('video-rotate', [90, 180, 270, 0])
+    })
+  }
+}
+module.exports.video = new Video()
