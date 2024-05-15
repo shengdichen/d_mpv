60df286deceb956817ed302bc30c7998482e52b8
shc <sh@c>
2024-05-15 15:39:21 +0200

    feat(setup): add setup.sh

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env dash
+
+SCRIPT_PATH="$(realpath "$(dirname "${0}")")"
+cd "${SCRIPT_PATH}" || exit 3
+
+DIR_MPV="${HOME}/.config/mpv"
+
+__misc() {
+    mkdir -p "${DIR_MPV}/scripts/"
+    (
+        cd "./js/" && npm install
+    )
+}
+
+__stow() {
+    stow -R --target="${HOME}" "raw"
+    (
+        cd "./js/" || exit 3
+        stow -R --target="${DIR_MPV}/" "src"
+    )
+}
+
+__misc
+__stow
