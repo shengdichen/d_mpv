86d0f2389a152ac862c2bde875f54be89a10ab36
shc <sh@c>
2024-05-18 00:42:01 +0200

    chore(util): typehint

--- js/src/js/lib/util.js
+++ js/src/js/lib/util.js
@@ -1,6 +1,12 @@
 var util = {
   raw: mp, // eslint-disable-line no-undef
 
+  /**
+   * @param {string} key
+   * @param {function(): void} fn
+   * @param {boolean} [repeatable]
+   * @param {boolean} [force]
+   */
   bind: function (key, fn, repeatable, force) {
     var flags = {};
     flags.repeatable = typeof repeatable !== "undefined" ? repeatable : true;
@@ -11,21 +17,36 @@ var util = {
     }
   },
 
+  /**
+   * @param {string} text
+   * @param {number} [duration]
+   */
   print_osd: function (text, duration) {
     duration = duration ? duration : 0.7;
     this.raw.osd_message(text, duration);
   },
 
+  /**
+   * @param {Array.<string>} fragments
+   */
   run: function (fragments) {
     this.raw.commandv.apply(null, fragments);
   },
 
+  /**
+   * @param {string} script
+   * @param {string} bind
+   */
   run_script_bind: function (script, bind) {
     // REF:
     //  https://mpv.io/manual/master/#command-interface-script-binding
     this.run(["script-binding", script + "/" + bind]);
   },
 
+  /**
+   * @param {string} fn
+   * @param {Array.<string>} args
+   */
   run_script_fn: function (fn, args) {
     this.run(["script-message", fn].concat(args));
   },
@@ -52,6 +73,11 @@ var util = {
     return this.raw.get_property_native(prop, def);
   },
 
+  /**
+   * @param {string} prop
+   * @param {*} val
+   * @param {string} type
+   */
   set_prop: function (prop, val, type) {
     if (type === "bool") {
       return this.raw.set_property_bool(prop, val);
@@ -65,6 +91,10 @@ var util = {
     return this.raw.set_property_native(prop, val);
   },
 
+  /**
+   * @param {string} item
+   * @param {Array.<*>} type
+   */
   cycle: function (item, values) {
     if (!values) {
       this.run(["cycle", item]);
@@ -73,6 +103,11 @@ var util = {
     }
   },
 
+  /**
+   * @param {string} prop
+   * @param {string} type
+   * @param {*} def
+   */
   print_prop: function (prop, type, def) {
     this.print_osd(this.get_prop(prop, type, def));
   },
