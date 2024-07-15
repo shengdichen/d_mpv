d215c4d5050a84634b9a13a8231f7008c29059ae
shc <sh@c>
2024-07-15 22:12:09 +0200

    feat(shell): add default (watch-later aware) open'g

--- raw/.local/script/mpv.sh
+++ raw/.local/script/mpv.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env dash
 
+WATCHLATER_DIR="${HOME}/.local/state/mpv/watch_later"
+
 . "${HOME}/.local/lib/util.sh"
 
 __mpv() {
@@ -11,6 +13,51 @@ __mpv_record() {
         --save-position-on-quit \
         --resume-playback \
         "${@}"
+}
+
+__mpv_default() {
+    if [ "${1}" = "--" ]; then shift; fi
+
+    local _n_watchlaters=0 _fname="" _f_watchlater=""
+    for _f in "${@}"; do
+        _fname="$(basename "$(realpath "${_f}")")"
+        if grep --recursive --quiet "^# ${_fname}$" "${WATCHLATER_DIR}"; then
+            _f_watchlater="${_f}"
+            _n_watchlaters=$((_n_watchlaters + 1))
+        fi
+    done
+
+    if [ "${_n_watchlaters}" -eq 0 ]; then
+        __mpv -- "${@}"
+    elif [ "${_n_watchlaters}" -eq 1 ]; then
+        printf "mpv> history found; what's the play?  [%s]\n" "${_f_watchlater}"
+        case "$(__fzf_opts "history" "blank")" in
+            "history")
+                printf "mpv> continuing history\n\n"
+                __mpv_record -- "${@}"
+                ;;
+            "blank")
+                printf "mpv> starting new\n\n"
+                __mpv -- "${@}"
+                ;;
+        esac
+    else
+        printf "mpv> multiple files with history found; what now?\n"
+        case "$(__fzf_opts "blank" "quit")" in
+            "blank")
+                printf "mpv> using none... "
+                read -r _
+                __mpv -- "${@}"
+                printf "\n\n"
+                ;;
+            "quit")
+                printf "mpv> quitting... "
+                read -r _
+                printf "\n\n"
+                return
+                ;;
+        esac
+    fi
 }
 
 __mpv_paste() {
@@ -59,5 +106,8 @@ case "${1}" in
     "paste")
         shift
         __mpv_paste
+        ;;
+    *)
+        __mpv_default "${@}"
         ;;
 esac
