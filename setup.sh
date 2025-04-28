2066a5ed35c1072b2bab903b0d5f65e9b2149045
shc <sh@c>
2025-04-28 18:59:46 +0200

    chore(setup): remove unnecessary globals

--- setup.sh
+++ setup.sh
@@ -3,30 +3,31 @@
 SCRIPT_PATH="$(realpath "$(dirname "${0}")")"
 cd "${SCRIPT_PATH}" || exit 3
 
-DIR_MPV="${HOME}/.config/mpv"
-
-DIR_MPD_LIB="${HOME}/.config/mpd/bin/lib"
 XDG_AUD="$(xdg-user-dir MUSIC | head -c -2)"
 
 __mpv() {
-    mkdir -p "${DIR_MPV}"
-    mkdir -p "${DIR_MPV}/scripts"
+    local _dir_config="${HOME}/.config/mpv"
+
+    mkdir -p "${_dir_config}"
+    mkdir -p "${_dir_config}/scripts"
 
     (
         cd "./common/js" || exit 3
-        stow -R --target "${DIR_MPV}" "src"
+        stow -R --target "${_dir_config}" "src"
         [ ! -d "./node_modules" ] && npm install
     )
     (
-        cd "./common" && stow -R --target "${DIR_MPV}" "mpv"
+        cd "./common" && stow -R --target "${_dir_config}" "mpv"
     )
 }
 
 __misc() {
     __mpd() {
-        mkdir -p "${DIR_MPD_LIB}"
-        if [ -z "$(ls -A "${DIR_MPD_LIB}")" ]; then
-            ln -s "${XDG_AUD}/a" "${DIR_MPD_LIB}/."
+        local _dir_lib="${HOME}/.config/mpd/bin/lib"
+
+        mkdir -p "${_dir_lib}"
+        if [ -z "$(ls -A "${_dir_lib}")" ]; then
+            ln -s "${XDG_AUD}/a" "${_dir_lib}/."
         fi
     }
 
